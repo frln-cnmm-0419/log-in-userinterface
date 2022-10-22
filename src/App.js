6e9c92d4bda7c0ddaeaf9dccf9df5e309693b56a
froilan <61849282+frln-cnmm-0419@users.noreply.github.com>
2022-10-22 14:06:24 +0000

    deployment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
-import welcome from "./components/Welcome";
+import Welcome from "./components/Welcome";
 import login from "./components/login";
 import error from "./components/error";
 import './styling/global.scss';
-import './styling/app.scss';
 import { BrowserRouter as Routes, Router, Route } from "react-router-dom";
 
 
 function App() {
   return  (
 		<div className="App">
-			<Router>
-				<Routes>
-				  <Route path="/" component={<welcome />}/>
-				  <Route path="/login" component={<login />}/>
-				  <Route path="*" component={<error />}/>
-			  </Routes>
-			</Router>
+			<Welcome />
 		</div>
 	);
 }
