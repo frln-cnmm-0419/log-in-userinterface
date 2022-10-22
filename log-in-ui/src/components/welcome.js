7f14ed5f0323321d4549997f4d4b2bdbb220f3ee
froilan <61849282+frln-cnmm-0419@users.noreply.github.com>
2022-10-22 12:35:38 +0800

    layout

--- log-in-ui/src/components/welcome.js
+++ log-in-ui/src/components/welcome.js
@@ -2,12 +2,20 @@ import React from "react";
 import "../styling/global.scss";
 import "../styling/welcome.scss"
 import ReactDOM from "react-dom";
+import google_icon from "../assets/google.png";
+import faecbook_icon from "../assets/facebook.png";
+import apple_icon from "../assets/apple.png";
+import banner from "../assets/banner.jpg";
 import { Switch, Route } from "react-router-dom";
 
 const welcome = () => {
 	return (
 		<div className="welcome">
-			<h1>Your all in one task organizer!</h1>
+			<img src={banner} alt="banner" />
+			<div className="title">
+				<h1>Your all in one task organizer!</h1>
+				<h6>Get started by logging in or signing up.</h6>
+			</div>
 			<div className="buttons">
 				<button>Log In</button>
 				<button>Create an Account</button>
